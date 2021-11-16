a3f81b24ca1b82a591fee159774eebab632a4516
Shovan61 <shovonmazumder61@gmail.com>
2021-11-17 02:49:11 +0600

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { HomePage, LoginPage } from "./Pages";
 
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route exact path="/" render={() => <LoginPage />}></Route>
+      <Route exact path="/home" render={() => <HomePage />}></Route>
+    </Switch>
+  );
 }
 
 export default App;
+
+// 6369f30dc672e06fc293aceafd86d8d6
+// https://api.themoviedb.org/3/movie/550?api_key=6369f30dc672e06fc293aceafd86d8d6
