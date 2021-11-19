100d1531693e8dd6232074c97b270e09c75ea24d
Shovan61 <shovonmazumder61@gmail.com>
2021-11-20 02:30:06 +0600

    added

--- my-app/src/Pages/LoginPage.jsx
+++ my-app/src/Pages/LoginPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import backimage from "../images/backimage.jpg";
 
 function LoginPage() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <DarkShadow></DarkShadow>
+    </Wrapper>
+  );
 }
 
 export default LoginPage;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: url(${backimage});
+  /* background-position: center center; */
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const DarkShadow = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.9),
+    rgba(42, 42, 42, 0.2) 60%,
+    rgba(0, 0, 0, 0.9) 100%
+  );
+`;
--- my-app/src/helper.js
+++ my-app/src/helper.js
@@ -13,21 +13,8 @@ export const getArr = (obj) => {
     }
   });
 
-  // console.log(allMovies);
   random = allMovies[Math.floor(Math.random() * allMovies.length)];
   return random;
-  // function randomMovie() {
-  //   let random;
-  //   random = arr[Math.floor(Math.random() * arr.length)];
-
-  //   if (random && random.backdrop_path !== null) {
-  //     return random;
-  //   } else if (random && random.backdrop_path === null) {
-  //     randomMovie();
-  //   }
-  // }
-  // let result = randomMovie();
-  // return result;
 };
 
 export const imageBaseUrl = "https://image.tmdb.org/t/p/original";
